68ae19c0449e108d129a747e9b7d9fd6e80df1b3
Jane Kim <jane.kim0717@gmail.com>
2022-03-16 13:42:42 -0400

    commit tree a0ba37f2de0ee8dc09d5b3f04c2465a7d0b85e3c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_reader :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    validate_triangle
+    if length1 == length2 && length2 == length3
+      :equilateral
+    elsif length1 == length2 || length2 == length3 || length1 == length3
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+  
+  def no_size?
+    [length1, length2, length3].all?(&:positive?)
+  end 
+
+  def triangle_inequality?
+    length1 + length2 > length3 && length1 + length3 > length2 && length2 + length3 > length1
+  end
+
+  def validate_triangle
+    raise TriangleError unless no_size? && triangle_inequality?
+  end
+
+    
+  class TriangleError < StandardError
+  end
+
 end
